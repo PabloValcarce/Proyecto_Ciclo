99e58d442b25fcabdad08effcf5d055dd6f4b9e8
PabloValcarce <pvalca99@gmail.com>
2022-04-23 17:30:25 +0200

    empezamos

--- JS/menu.js
+++
@@ -1,6 +0,0 @@
-let categorias = document.getElementById('categorias');
-let categoria = document.getElementById('categoria');
-
-categorias.addEventListener('click', ()=>{
-    categoria.style.display='block';
-})
