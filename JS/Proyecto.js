933079e2b6f4d006b80e9687a2ed761ecad83547
PabloValcarce <pvalca99@gmail.com>
2022-04-26 21:05:41 +0200

    opacidad

--- JS/Proyecto.js
+++ JS/Proyecto.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".nav-toggle")
 const navMenu = document.querySelector(".nav-menu")
+const scroll_1= document.getElementById("scroll_1")
+const scroll_2= document.getElementById("scroll_2")
 
 navToggle.addEventListener("click",() => {
     navMenu.classList.toggle("nav-menu-visible");
+})
+window.addEventListener("scroll",()=>{
+    scroll_1.style.opacity="1";
+    scroll_2.style.opacity="1";
 })
