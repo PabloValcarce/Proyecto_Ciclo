dd060fc292f5ef95962a415b5bfe6bc02734d9c8
PabloValcarce <pvalca99@gmail.com>
2022-05-23 19:25:39 +0200

    cosas

--- PHP/config.php
+++ PHP/config.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $pass = "";
-    $database = "tienda";
+    $server = "mysql.webcindario.com";
+    $user = "frigelu";
+    $pass = "Frigelu123.";
+    $database = "frigelu";
 
     $conn  = mysqli_connect($server,$user,$pass,$database);
     if(!$conn){
